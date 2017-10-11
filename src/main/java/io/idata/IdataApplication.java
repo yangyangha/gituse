1980c1dca40967a06f069d4ac87f531b64ef5c62
yangyangha <cy152150136@163.com>
2017-10-11 17:50:50 +0800

    update

--- src/main/java/io/idata/IdataApplication.java
+++ src/main/java/io/idata/IdataApplication.java
@@ -1,24 +1,9 @@
 package io.idata;
 
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
-@MapperScan("io.idata.dao")
-
-@EnableAutoConfiguration(exclude={
-		DataSourceAutoConfiguration.class,
-		DataSourceTransactionManagerAutoConfiguration.class
-})
-@EnableTransactionManagement
 public class IdataApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IdataApplication.class, args);
+		System.out.println("hello");
 	}
 }
