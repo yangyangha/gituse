02a1561c5d1e6e980f802a1ac442b87619f04c82
ningmengwork <ningmengwork@126.com>
2018-04-09 21:01:43 +0800

    commit message

---
+++ src/main/java/io/idata/IdataApplication.java
@@ -0,0 +1,15 @@
+package io.idata;
+
+
+public class IdataApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+	//pushed first
+
+    //again
+
+    //3
+}
